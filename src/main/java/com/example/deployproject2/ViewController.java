a140e15ef7e6291dc42f53dd1784149349bfcc8d
DESKTOP-HE2PQ81\macdi <macdinhphap123@gmail.com>
2023-06-23 21:17:37 +0700

    update person

--- src/main/java/com/example/deployproject2/ViewController.java
+++ src/main/java/com/example/deployproject2/ViewController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -15,5 +16,10 @@ public class ViewController {
     @GetMapping("/index")
     public String index(){
         return cartRepository.findAll().toString();
+    }
+    @ResponseBody
+    @GetMapping("/person/{id}")
+    public String home(@PathVariable("id") int id){
+        return cartRepository.findById(id).get().toString();
     }
 }
