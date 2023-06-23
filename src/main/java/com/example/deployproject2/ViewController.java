cbe2543279ef62aca260012f756d54c0eb87dac0
DESKTOP-HE2PQ81\macdi <macdinhphap123@gmail.com>
2023-06-23 20:25:09 +0700

    main

--- src/main/java/com/example/deployproject2/ViewController.java
+++ src/main/java/com/example/deployproject2/ViewController.java
@@ -1,12 +1,19 @@
 package com.example.deployproject2;
 
+import com.example.deployproject2.repositories.CartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ViewController {
+    @Autowired
+    private CartRepository cartRepository;
+    @ResponseBody
     @GetMapping("/index")
     public String index(){
-        return "index";
+        return cartRepository.findAll().toString();
     }
 }
