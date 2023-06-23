40ed63cd25c228bd8ae4ec9519d064b70581dcde
DESKTOP-HE2PQ81\macdi <macdinhphap123@gmail.com>
2023-06-23 14:39:45 +0700

    main

---
+++ src/main/java/com/example/deployproject2/DeployProject2Application.java
@@ -0,0 +1,13 @@
+package com.example.deployproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployProject2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeployProject2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/deployproject2/DeployProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deployproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployProject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
